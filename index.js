c5acfba34497b6ee1ca8a710a126c7c1af51f3ed
Selma Mokadem <selmamokadem@MacBook-Air-de-Selma.local>
2022-11-23 13:52:00 +0100

    Home Page Done

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
-const btn = document.querySelector(".btn");
-const btnInscription = document.querySelector('.inscr')
-const text = document.querySelector(".text");
-const text2 = document.querySelector(".text2");
+// const btn = document.querySelector(".btn");
+// const btnInscription = document.querySelector('.inscr')
+// const text = document.querySelector(".text");
+// const text2 = document.querySelector(".text2");
 
-let shown = true;
-let shown2 = true;
+// let shown = true;
+// let shown2 = true;
 
 
-function toggleText() {
-    if(shown) {
-        btn.innerText = "connextion";
-        text.style.opacity = 0;
-    }
+// function toggleText() {
+//     if(shown) {
+//         btn.innerText = "connextion";
+//         text.style.opacity = 0;
+//     }
     
-    else {
-       btn.innerText = "retour en arriere";
-       text.style.opacity = 1;
-    }
+//     else {
+//        btn.innerText = "retour en arriere";
+//        text.style.opacity = 1;
+//     }
     
-    shown = !shown
-}
+//     shown = !shown
+// }
 
-btn.addEventListener("click", () => {
-    toggleText();
-})
+// btn.addEventListener("click", () => {
+//     toggleText();
+// })
 
 
 
-function toggleText2() {
-    if(shown2) {
-        btnInscription.innerText = "inscription";
-        text2.style.opacity = 0;
-    }
+// function toggleText2() {
+//     if(shown2) {
+//         btnInscription.innerText = "inscription";
+//         text2.style.opacity = 0;
+//     }
     
-    else {
-       btnInscription.innerText = "retour en arriere";
-       text2.style.opacity = 1;
-    }
+//     else {
+//        btnInscription.innerText = "retour en arriere";
+//        text2.style.opacity = 1;
+//     }
     
-    shown2 = !shown2
-}
+//     shown2 = !shown2
+// }
 
-btnInscription.addEventListener("click", () => {
-    toggleText2();
-})
+// btnInscription.addEventListener("click", () => {
+//     toggleText2();
+// })
